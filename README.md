6eaebd2710a4ca937b79a914c4e13f3533a6ab96
escyezi <escyezi@foxmail.com>
2020-11-29 12:09:02 +0800

    todo移动到readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# my-excel-exporter
+## todo list
+##### todo 调整表格输出的样式
+- 根据字段长度调整行宽
+- 首个sheet增加跳转到其他sheet的链接
+
+##### todo 报错提示友好化
+##### todo 打印进度信息
+##### todo 增加注释
+##### todo 增加README
+##### todo 打zip包作为release
--- src/main.go
+++ src/main.go
@@ -6,14 +6,6 @@ import (
 	"os"
 )
 
-//todo 调整表格输出的样式
-//根据字段长度调整行宽
-//首个sheet增加跳转到其他sheet的链接
-
-//todo 报错提示友好化
-//todo 打印进度信息
-//todo 提交到GitHub
-//todo 打zip包作为release
 func main() {
 	cwd, _ := os.Getwd()
 	config := util.ReadConfig(cwd + "/conf/config.json")
