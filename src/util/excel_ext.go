494df7b7f28971abb31521afa79a148b645207d1
escyezi <escyezi@foxmail.com>
2020-11-30 12:41:42 +0800

    设置超链接样式以及处理表名超长截断导致链接无效的问题

--- src/util/excel_ext.go
+++ src/util/excel_ext.go
@@ -2,19 +2,22 @@ package util
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
 type TableListSheetExcelUtil struct {
-	e                     *ExcelUtil
+	excelUtil             *ExcelUtil
 	tableListSheetName    string //表清单页的sheet名
 	tableNameColCharIndex string //表名列的列号 如"A"
 	tableNameColIndex     int    //表名列的列数 如0
+	hyperLinkStyleId      int    //超链接样式id
 }
 
 //ColNames 表清单sheet的列名
 //tableColName 表名列的列名
 func NewTableListSheetExcelUtil(filePath string, tableListSheetName string, ColNames []string, tableColName string) *TableListSheetExcelUtil {
+	excelUtil := NewExcelUtil(filePath)
 	tableNameColIndex := func() int {
 		for i := 0; i < len(ColNames); i++ {
 			if ColNames[i] == tableColName {
@@ -23,43 +26,61 @@ func NewTableListSheetExcelUtil(filePath string, tableListSheetName string, ColN
 		}
 		panic(fmt.Sprintf("%s does not exist in %s", tableColName, ColNames))
 	}()
+	styleId, err := excelUtil.excel.NewStyle(`{"font":{"underline":"single","color":"#0066CC"}}`)
+	PanicIfErr(err)
 	return &TableListSheetExcelUtil{
-		e:                     NewExcelUtil(filePath),
+		excelUtil:             excelUtil,
 		tableListSheetName:    tableListSheetName,
 		tableNameColIndex:     tableNameColIndex,
 		tableNameColCharIndex: string(rune('A' + tableNameColIndex)),
+		hyperLinkStyleId:      styleId,
 	}
 }
 func (teu *TableListSheetExcelUtil) GetExcelUtil() *ExcelUtil {
-	return teu.e
+	return teu.excelUtil
 }
 
 //列表页设置跳转到表的超链接
 //columnName是表名所在的列名，即sql文件里设置的别名或字段名
 func (teu *TableListSheetExcelUtil) SetHyperLinksToTableSheet() {
-	excel := teu.e.excel
+	excel := teu.excelUtil.excel
 	rows, err := excel.Rows(teu.tableListSheetName)
 	PanicIfErr(err)
-	rowNum := 1
+
+	//第一行是列名，不设置超链接
+	if !rows.Next() {
+		return
+	}
+	rowNum := 2
 	for rows.Next() {
 		tableName := rows.Columns()[teu.tableNameColIndex]
-		excel.SetCellHyperLink(teu.tableListSheetName,
-			teu.tableNameColCharIndex+strconv.Itoa(rowNum),
-			tableName+"!A1",
-			"Location")
+		axis := teu.tableNameColCharIndex + strconv.Itoa(rowNum)
+
+		hyperLinkSheetName := tableName
+		if len(tableName) > 31 {
+			fmt.Printf("[WARNING] the size of table name \"%s\" is large than 31,will cut off by excel",
+				tableName)
+			hyperLinkSheetName = tableName[0:31]
+		}
+		link := hyperLinkSheetName + "!A1"
+
+		excel.SetCellHyperLink(teu.tableListSheetName, axis, link, "Location")
+		excel.SetCellStyle(teu.tableListSheetName, axis, axis, teu.hyperLinkStyleId)
 		rowNum++
 	}
 }
 
 //各个表设置跳转到表清单sheet的超链接
 func (teu *TableListSheetExcelUtil) SetHyperLinksToTableList() {
-	excel := teu.e.excel
+	excel := teu.excelUtil.excel
 	for _, sheetName := range excel.GetSheetMap() {
 		if sheetName == teu.tableListSheetName {
 			continue
 		}
-		excel.SetCellHyperLink(sheetName, "A1", teu.tableListSheetName+"!A1", "Location")
+		hyperLinkSheetName := sheetName[0:int(math.Min(float64(len(sheetName)), 31))]
+		excel.SetCellHyperLink(hyperLinkSheetName, "A1", teu.tableListSheetName+"!A1", "Location")
 		cellValue := excel.GetCellValue(sheetName, "A1")
 		excel.SetCellValue(sheetName, "A1", cellValue+"(回到首页)")
+		excel.SetCellStyle(sheetName, "A1", "A1", teu.hyperLinkStyleId)
 	}
 }
